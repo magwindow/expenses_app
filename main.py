1a476918ea8ded7d55c2f8dcb41247b92a98dcfc
magwindow <antosha.petrakov@yandex.ru>
2025-09-29 23:31:35 +0300

    Added frame

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import tkinter as tk
 window = tk.Tk()
 window.title('My App')
 
+frame_add_form = tk.Frame(window, width=150, height=150, bg='green')
+frame_statistic = tk.Frame(window, width=150, height=150, bg='yellow')
+frame_list = tk.Frame(window, width=300, height=150, bg='blue')
+
+frame_add_form.grid(row=0, column=0)
+frame_statistic.grid(row=0, column=1)
+frame_list.grid(row=1, column=0, columnspan=2)
+
 window.mainloop()
