215727f41eb58a0cb798e9953f073377c395cd43
magwindow <antosha.petrakov@yandex.ru>
2025-09-30 21:48:27 +0300

    Added switch language in app

--- app.py
+++ app.py
@@ -16,7 +16,12 @@ class App(tk.Tk):
         self.style.configure("SmplLbl.TLabel", padding=(10, 10, 60, 10))
         self.style.configure("BldLbl.TLabel", font=("Helvetica", 14, "bold"), padding=(0, 10, 0, 10))
         self['background'] = "#EBEBEB"
+
         self.put_frames()
+        self.put_menu()
+
+    def put_menu(self):
+        self.config(menu=MainMenu(self))
 
     def put_frames(self):
         self.add_form_frame = AddForm(self).grid(row=0, column=0, sticky="nswe")
@@ -28,6 +33,47 @@ class App(tk.Tk):
         for f_name in all_frames:
             self.nametowidget(f_name).destroy()
         self.put_frames()
+        self.put_menu()
+
+    def quit(self):
+        self.destroy()
+
+    def switch_lang(self, language):
+        self.lang = language
+        self.schema = eh.get_lang_schema(language)
+        self.refresh()
+
+
+class MainMenu(tk.Menu):
+    def __init__(self, mainwindow):
+        super().__init__(mainwindow)
+
+        file_menu = tk.Menu(self)
+        options_menu = tk.Menu(self)
+        help_menu = tk.Menu(self)
+
+        file_menu.add_command(label="Refresh", command=mainwindow.refresh)
+        file_menu.add_separator()
+        file_menu.add_command(label="Quit", command=mainwindow.quit)
+
+        lang_menu = tk.Menu(options_menu)
+        self.s_var = tk.StringVar()
+        self.s_var.set(self.master.lang)
+        lang_menu.add_radiobutton(label="English", variable=self.s_var, value="en",
+                                  command=lambda: mainwindow.switch_lang(self.s_var.get()))
+        lang_menu.add_radiobutton(label="Русский", variable=self.s_var, value="ru",
+                                  command=lambda: mainwindow.switch_lang(self.s_var.get()))
+
+        options_menu.add_cascade(label="Switch language", menu=lang_menu)
+        options_menu.add_separator()
+        options_menu.add_command(label="Switch theme")
+        help_menu.add_command(label="About Us")
+        help_menu.add_separator()
+        help_menu.add_command(label="FAQ")
+
+        self.add_cascade(label="File", menu=file_menu)
+        self.add_cascade(label="Options", menu=options_menu)
+        self.add_cascade(label="Help", menu=help_menu)
 
 
 class AddForm(tk.Frame):
